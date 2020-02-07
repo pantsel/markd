2d874fb34ac9e5052b4d0c88fb274547da23dc79
FB72MV <panagis.tselentis@ing.com>
2020-02-07 10:06:00 +0100

    ref

--- markd/main/tests/unit.py
+++ markd/main/tests/unit.py
@@ -1,4 +1,9 @@
-import unittest, os, time, sys, shutil
+""" Package tests	"""
+
+import unittest
+import os
+import sys
+import shutil
 
 PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if not PATH in sys.path:
@@ -8,8 +13,14 @@ del PATH
 
 class TestReports(unittest.TestCase):
 
+    """ Package tests	"""
+
     def setUp(self):
-        self.output_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'test-output'))
+        """
+        Setup tests
+        """
+        folder = os.path.join(os.path.dirname(__file__), '..', 'test-output')
+        self.output_folder = os.path.abspath(folder)
         self.trace = '''
         Traceback (most recent call last):
         File "t.py", line 6, in <module>
@@ -18,21 +29,27 @@ class TestReports(unittest.TestCase):
         '''
 
     def tearDown(self):
+        """
+        Remove test folders after testing
+        """
         shutil.rmtree(self.output_folder)
-    
+
     def test_markdown_created(self):
+        """
+        Test that the markdown file was created
+        """
         filename = self.output_folder + "/test.md"
-        o = Markdown()
-        o.add_header("This is a report h1")
-        o.add_header("This is an h2", 2)
-        o.add_text("Lorem ipsum text for the summary just to check how it works and if its possible")
-        o.add_blockquote("You can also add blockquotes!!")
-        o.add_code(self.trace)
-        o.add_horizontal_rule()
-        o.save(filename)
+        output = Markdown()
+        output.add_header("This is a report h1")
+        output.add_header("This is an h2", 2)
+        output.add_text("Lorem ipsum text for the summary")
+        output.add_blockquote("You can also add blockquotes!!")
+        output.add_code(self.trace)
+        output.add_horizontal_rule()
+        output.save(filename)
         self.assertTrue(os.path.isdir(self.output_folder))
         self.assertTrue(os.path.isfile(filename))
-    
+
 
 if __name__ == '__main__':
     unittest.main()
